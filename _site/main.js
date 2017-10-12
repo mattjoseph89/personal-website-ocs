7af35683d14f506e85c47131ebc5c79597f3b054
Matt Joseph Wurth <mattjwurth@gmail.com>
2017-10-12 14:20:05 -0500

    workin on it

---
+++ _site/main.js
@@ -0,0 +1,31 @@
+// navigation drawer dropdown--------------------------------------------
+
+window.onload = function() {
+
+  document.querySelector('.toggleNav').addEventListener('click', function() {
+    var navBar = document.querySelector('nav');
+    if (navBar.className === 'clearfix') {
+        navBar.className = 'clearfix none';
+        var changezIndex = setTimeout(function() {
+        navBar.style.zIndex = 1;
+        }, 200);
+    }
+    else {
+      navBar.style.zIndex = 0;
+      navBar.className = 'clearfix';
+    };
+  });
+
+
+  var $window = $(window);
+  $window.on('resize', function (){
+    var navBar = document.querySelector('nav');
+      if ($window.width() > 600){
+        navBar.className = 'clearfix none';
+        var changezIndex = setTimeout(function() {
+        navBar.style.zIndex = 1;
+      }, 200);
+    };
+  });
+  
+}
